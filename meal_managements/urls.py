cdb39ae2cd016f6402fea4e8efcf05fc0bd2b78e
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-12-10 10:50:45 +0600

    select group page add

--- meal_managements/urls.py
+++ meal_managements/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('select-group', views.select_group, name='select_group'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
--- meal_managements/views.py
+++ meal_managements/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
+def select_group(request):
+    return render(request, 'meal_managements/select_group.html', {'title': 'Meal Manager'})
+
+
 def dashboard(request):
     return render(request, 'meal_managements/dashboard.html', {'title': 'Dashboard'})
