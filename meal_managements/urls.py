5ea7b5598b023895eef510de3ab2e26806ed77eb
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-12-10 16:03:36 +0600

    Create group page add

--- meal_managements/urls.py
+++ meal_managements/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('select-group', views.select_group, name='select_group'),
+    path('create-group', views.create_group, name='create_group'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
--- meal_managements/views.py
+++ meal_managements/views.py
@@ -5,5 +5,9 @@ def select_group(request):
     return render(request, 'meal_managements/select_group.html', {'title': 'Meal Manager'})
 
 
+def create_group(request):
+    return render(request, 'meal_managements/group/create_group.html', {'title': 'Create Group'})
+
+
 def dashboard(request):
     return render(request, 'meal_managements/dashboard.html', {'title': 'Dashboard'})
