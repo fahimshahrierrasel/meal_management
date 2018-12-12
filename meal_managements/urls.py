a0480bff314726b25a1984487ab66f9b821a5eb2
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2018-12-13 00:55:47 +0600

    select_group.html moved and join_group.html add

--- meal_managements/urls.py
+++ meal_managements/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('select-group', views.select_group, name='select_group'),
     path('create-group', views.create_group, name='create_group'),
+    path('join-group', views.join_group, name='join_group'),
     path('dashboard', views.dashboard, name='dashboard'),
 ]
--- meal_managements/views.py
+++ meal_managements/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 
 
 def select_group(request):
-    return render(request, 'meal_managements/select_group.html', {'title': 'Meal Manager'})
+    return render(request, 'meal_managements/group/select_group.html', {'title': 'Meal Manager'})
 
 
 def create_group(request):
     return render(request, 'meal_managements/group/create_group.html', {'title': 'Create Group'})
+
+
+def join_group(request):
+    return render(request, 'meal_managements/group/join_group.html', {'title': 'Join Group'})
 
 
 def dashboard(request):
